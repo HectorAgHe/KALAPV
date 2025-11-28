86e69ecfbcc365e42c1a9ef5f240d2aeefe3f09e
HectorAguilar <aguilarhector440@gmail.com>
2025-11-28 00:18:38 -0600

    Agregar archivos de proyecto.

---
+++ KalaPV.Application/Features/Categories/CategoryProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalaPV.Application.Features.Categories
+{
+    internal class CategoryProfile
+    {
+    }
+}
