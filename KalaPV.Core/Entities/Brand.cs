5e1bee6a49ce7b00ceab97dbeb7b4e5005069e0a
HectorAgHe <aguilarhector440@gmail.com>
2026-01-24 19:32:34 -0600

    Prueba de commit Feat: agrega entidad

--- KalaPV.Core/Entities/Brand.cs
+++ KalaPV.Core/Entities/Brand.cs
@@ -6,18 +6,10 @@ using System.Threading.Tasks;
 
 namespace KalaPV.Core.Entities
 {
-    public class Products
+    public class Brand
     {
-        public int ProductId { get; set; }        
+        public int BrandId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-        public int Stock { get; set; }    
-        
-        public int CategoryId { get; set; }
-        public int BrandId { get; set; }
-
-
-
     }
 }
